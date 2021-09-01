45bfd9a26835c9b45bc061120db2b06305b5f2a4
Ezequiel A. Freire <ezequiel.a.freire@gmail.com>
2021-09-01 16:58:04 -0300

    messages in response

--- network/response.js
+++ network/response.js
@@ -1,16 +1,19 @@
 exports.success = (req, res, message, status) => {
-    req.status(status).send({
+    let statusCode = status || 200;
+    let statusMessage = message || '';
+    res.status(statusCode).send({
         error: false,
-        status: status,
-        body: message
+        status: statusCode,
+        body: statusMessage
     });
 }
 
 exports.error = (req, res, message, status) => {
-    let statusCode = status || 500
-    req.status(statusCode).send({
+    let statusCode = status || 500;
+    let stausMessage = message || 'internal error'
+    res.status(statusCode).send({
         error: false,
-        status: status,
-        body: message
+        status: statusCode,
+        body: stausMessage
     });
 }
