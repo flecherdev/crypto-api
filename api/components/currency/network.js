a6b4e7cb02812a1609d2cf125b9f3d51ba510233
Ezequiel A. Freire <ezequiel.a.freire@gmail.com>
2021-09-01 17:18:12 -0300

    added response to index

--- api/components/currency/network.js
+++ api/components/currency/network.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
+const response = require('../../../network/response')
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Todo funciona');
+    response.success(req, res, 'response success', 200)
 });
 
 module.exports = router
