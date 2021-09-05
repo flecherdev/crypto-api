036d0fefd9d05e40b46bacd67fefd8cc7af0bc57
Ezequiel A. Freire <ezequiel.a.freire@gmail.com>
2021-09-05 16:19:43 -0300

    route to currency

--- api/components/currency/network.js
+++ api/components/currency/network.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const response = require('../../../network/response')
+const controller = require('./index')
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    response.success(req, res, 'response success', 200)
+    controller.list()
+        .then( (list) => {
+            response.success(req, res, list, 200)
+        }).catch( error => {
+            response.error(req, res, error.message, 500)
+        });
+});
+
+router.get('/:id', (req, res) => {
+    controller.get(req.params.id)
+        .then( (currency) => {
+            response.success(req, res, currency, 200)
+        }).catch( error => {
+            response.error(req, res, error.message, 500)
+        });
+});
+
+router.post('/', (req, res) => {
+    Controller.upsert(req.body)
+        .then( (currency) => {
+            response.success(req, res, currency, 201)
+        }).catch( error => {
+            response.error(req, res, error.message, 500)
+        });
 });
 
 module.exports = router
