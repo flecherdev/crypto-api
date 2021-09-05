7d478be0cea5d2225c12bc860cdb972d5fdd53bb
Ezequiel A. Freire <ezequiel.a.freire@gmail.com>
2021-09-05 16:20:58 -0300

    add use json an urlecoded for body

--- api/index.js
+++ api/index.js
@@ -3,8 +3,11 @@ const config = require('../config.js');
 const currency = require('./components/currency/network')
 const app = express()
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Router
-app.use('/currencies/', currency);
+app.use('/currencies', currency);
 
 app.listen(config.api.port, () => {
     console.log(`http://localhost:${config.api.port}`);
